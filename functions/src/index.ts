a00e5a8e880f50e1e330b224ab1e7d07bab88479
divyanshu <dgdivyanshu@gmail.com>
2020-01-13 23:52:58 +0530

    New

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,31 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-admin.initializeApp()
+admin.initializeApp({
+	storageBucket:'gs://securityapplication-b990e.appspot.com'
+})
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+//const fs= require('fs')
+export const testCount= functions.https.onRequest(async (req,res)=>{
+	const callerUid=req.query.callerUid
+	const targetUid=req.query.targetUid
+	console.log(`${callerUid} called the https trigger. Sending message to victim_${targetUid}`)
+
+	const payload={
+		data:{
+			uid:targetUid,
+			testCount:String(1),
+		}
+	}
+	const topic="victim_"+targetUid
+	await admin.messaging().sendToDevice(topic,payload)
+	.catch(err=>{console.log("error:"+err)
+				return res.status(500).send(err)
+		})
+	return res.status(200).send("success")
+})
 export const alertLocationOnUpdate = functions.database
 .ref('/alerts/{uid}/location')
 .onUpdate((change,context)=>{
@@ -13,18 +34,18 @@ export const alertLocationOnUpdate = functions.database
 	console.log(`"${uid} was updated with location ${location}. Previous location ${change.before.val()}"`)
 	//send data message with updated location
 	const payload= {
-					data:{
-						uid:uid,
-						liveLocation:location,
-					}
-				}
-				console.log(payload)
-				//return a Promise so that cloud functions wait till Promise is handled before cleaning-up
-				const topic="saviours_"+uid			
-				console.log("Topic received:"+topic+" type:"+typeof(topic))
-				
-				return admin.messaging().sendToTopic(topic, payload)
-				.then((resp)=>{console.log("Data message dispatched:"+resp)})	
+		data:{
+			uid:uid,
+			liveLocation:location,
+		}
+	}
+	console.log(payload)
+	//return a Promise so that cloud functions wait till Promise is handled before cleaning-up
+	const topic="saviours_"+uid			
+	console.log("Topic received:"+topic+" type:"+typeof(topic))
+
+	return admin.messaging().sendToTopic(topic, payload)
+	.then((resp)=>{console.log("Data message dispatched:"+resp)})	
 
 })
 
@@ -36,18 +57,18 @@ export const alertSavioursOnUpdate = functions.database
 	console.log(`"Alert ${uid} was updated with Saviour count: ${saviourCount}."`)
 	//send data message with updated location
 	const payload= {
-					data:{
-						uid:uid,
-						saviourCount:String(saviourCount),
-					}
-				}
-				console.log(payload)
-				//return a Promise so that cloud functions wait till Promise is handled before cleaning-up
-				const topic="victim_"+uid			
-				console.log("Topic received:"+topic+" type:"+typeof(topic))
-				
-				return admin.messaging().sendToTopic(topic, payload)
-				.then((resp)=>{console.log("Count Update Data message dispatched:"+resp)})	
+		data:{
+			uid:uid,
+			saviourCount:String(saviourCount),
+		}
+	}
+	console.log(payload)
+	//return a Promise so that cloud functions wait till Promise is handled before cleaning-up
+	const topic="victim_"+uid			
+	console.log("Topic received:"+topic+" type:"+typeof(topic))
+
+	return admin.messaging().sendToTopic(topic, payload)
+	.then((resp)=>{console.log("Count Update Data message dispatched:"+resp)})	
 
 })
 
@@ -110,6 +131,66 @@ export const alertOnCreate = functions.database
 	return null
 })
 
+export const alertOnDelete= functions.database
+.ref('/alerts/{uid}/')
+.onDelete( async (snapshot,context)=>{
+	const uid = context.params.uid
+	const ts= snapshot.val().ts
+	console.log("Alert by uid:"+uid+" with timestamp:"+ts+" was deleted")
+	/*const bucket = admin.storage().bucket()
+	const file=ts+".json"
+	const path="/"+uid+"/"+"alerts"+"/"+file
+	fs.writeFile(file,snapshot.exportVal(),function (err:any) {
+		if (err) {return err;}
+		console.log('File is created successfully.');
+	})
+	return bucket.upload(file,{
+		destination:path
+	})*/
+	const rootRef= snapshot.ref.root
+	if(rootRef!==null){
+		try{
+		await rootRef.child("alerthistory").push(snapshot.val()).child("uid").set(uid)
+		}
+		catch{
+			console.log("Error when pushing history to firebase");
+		}
+		const UsersRef= rootRef.child("Users")
+			const username= UsersRef.child(uid).ref
+			username.once("value").then(snashot => {
+				console.log(`"Json of the user is ${snashot.val()}"`)
+				const name=snashot.val().name
+				//Method to retrieve all keys of a node :const keys= Object.keys(snashot.val())
+				//console.log("keys"+keys)
+				console.log("Name:"+name)
+
+				//after name has been fetched send a message on that topic
+				//a data message Notification 
+				const payload= {
+					
+					data:{
+						username:name,
+						uid:uid,
+						safe:"true",
+						//imageUrl: To be implemented
+					}
+				}
+				console.log(payload)
+				//return a Promise so that cloud functions wait till Promise is handled before cleaning-up
+				var topic="saviours_"+uid			
+				console.log("Topic received:"+topic+" type:"+typeof(topic))
+				
+				return admin.messaging().sendToTopic(topic, payload)	
+
+			})
+			.catch( err=>{console.log("error:"+err)})
+	}
+
+	
+	//send message to saviours that the victim is now safe
+
+	return null
+})
 //return values as (lat,long) as long_lat i.e. if (19.4,2.3) => 19_2
 //input as dd gps string separated by comma i.e. "19.4,2.3"
 function getzone(location:String){
